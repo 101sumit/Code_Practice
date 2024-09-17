2c4e78d4da5931ba5c08b5782d423da68c085435
Sumit Patil <sumitpatil00563@gmail.com>
2024-09-17 15:34:06 +0530

    Add project files.

---
+++ Extension.cs
@@ -0,0 +1,37 @@
+﻿namespace Practice
+{
+    public class Base
+    {
+        public void Method_1()
+        {
+            Console.WriteLine("Method_1");
+        }
+        public void Method_2()
+        {
+            Console.WriteLine("Method_2");
+        }
+    }
+
+    public static class ExtensionClass
+    {
+        public static int Method_3(this Base obj, int i)    //Extension Method
+        {
+            Console.WriteLine("Method_3... i : " + (i + 1));
+            return ++i;
+        }
+    }
+
+    public class Extension
+    {
+        public static void Main(string[] args)
+        {
+            Base obj = new Base();
+            
+            obj.Method_1();
+            obj.Method_2();
+
+            int i = obj.Method_3(4);
+            Console.WriteLine(i);
+        }
+    }
+}
